f3dbab3c2c350243f7896c95eb19ecb6f97760e4
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-03 21:02:08 -0300

    Initial commit

---
+++ app/src/main/java/br/com/dhccorreia/minhabiblioteca/Calculadora.kt
@@ -0,0 +1,11 @@
+package br.com.dhccorreia.minhabiblioteca
+
+class Calculadora {
+    fun somar(v1: Int, v2: Int): Int {
+        return v1 + v2
+    }
+
+    fun subtrair(v1: Int, v2: Int): Int{
+        return v1 - v2
+    }
+}
